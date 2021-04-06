a960829827996b876dc264c0e675a0a20ac77d31
Alexey Shumilin <shumil.alexey@gmail.com>
2021-04-06 19:09:33 +0300

    Preloader
    added preloader to movies search

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import "./Movies.css";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import SearchForm from "../SearchForm/SearchForm";
 import Preloader from "../Preloader/Preloader";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
@@ -11,202 +11,201 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import shortMoviesHandle from "../../helpers/shortMovies";
 
-function Movies({isLogin}) {
-    const {pathname} = useLocation();
-    const [movies, setMoviesList] = React.useState([]);
-    const [renderedFilms, setRenderedFilms] = React.useState([]);
-    const [countClickMoreFilms, setCountClickMoreFilms] = React.useState(1);
-    const [searchValue, setSearchValue] = React.useState("");
-    const [inputError, setInputError] = React.useState("");
-    const [visibilityMoviesList, setVisibilityMoviesList] = React.useState("");
-    const [isPreloaderOpen, setIsPreloaderOpen] = React.useState("");
-    const [savedMovies, setSavedMovies] = React.useState([]);
-    const [visibilityBtnYet, setVisibilityBtnYet] = React.useState(
-        "movies__button_hidden"
+function Movies({ isLogin }) {
+  const { pathname } = useLocation();
+  const [movies, setMoviesList] = React.useState([]);
+  const [renderedFilms, setRenderedFilms] = React.useState([]);
+  const [countClickMoreFilms, setCountClickMoreFilms] = React.useState(1);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [inputError, setInputError] = React.useState("");
+  const [visibilityMoviesList, setVisibilityMoviesList] = React.useState("");
+  const [isPreloaderOpen, setIsPreloaderOpen] = React.useState("");
+  const [savedMovies, setSavedMovies] = React.useState([]);
+  const [visibilityBtnYet, setVisibilityBtnYet] = React.useState(
+    "movies__button_hidden"
+  );
+  const [isShortFilms, setIsShortFilms] = React.useState(false);
+
+  React.useEffect(() => {
+    MainApi.getSavedMovies()
+      .then((savedMoviesData) => {
+        if (savedMoviesData) {
+          setSavedMovies(savedMoviesData);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    if (pathname === "/saved-movies") {
+      setVisibilityMoviesList("movies_visibility");
+    }
+  }, []);
+
+  function filterMovies(films) {
+    if (isShortFilms) {
+      return shortMoviesHandle(films);
+    }
+    return films.filter((movie) => movie.duration >= 40);
+  }
+
+  function definitionSizeScreen() {
+    return document.documentElement.clientWidth;
+  }
+
+  function coefficientScreen() {
+    const width = definitionSizeScreen();
+    if (width >= 1280) {
+      return 3;
+    }
+    return 2;
+  }
+
+  const filteredMovies = React.useMemo(() => filterMovies(movies), [
+    isShortFilms,
+    movies,
+  ]);
+  const filteredRenderedMovies = React.useMemo(
+    () => filterMovies(renderedFilms),
+    [isShortFilms, renderedFilms]
+  );
+  const filteredSavedMovies = React.useMemo(() => filterMovies(savedMovies), [
+    isShortFilms,
+    savedMovies,
+  ]);
+
+  React.useEffect(() => {
+    if (filteredMovies.length >= filteredRenderedMovies.length) {
+      setVisibilityBtnYet("movies__button_hidden");
+    }
+  }, [filteredMovies, filteredRenderedMovies]);
+
+  function countInitCards() {
+    const width = definitionSizeScreen();
+    if (width >= 1280) {
+      return 12;
+    }
+    if (width >= 757) {
+      return 8;
+    }
+    return 5;
+  }
+
+  function handleMoreRenderFilms() {
+    const cards = countInitCards();
+
+    setRenderedFilms(
+      filteredMovies.slice(0, cards + countClickMoreFilms * coefficientScreen())
     );
-    const [isShortFilms, setIsShortFilms] = React.useState(false);
+    setCountClickMoreFilms(countClickMoreFilms + 1);
+  }
 
-    React.useEffect(() => {
-        MainApi.getSavedMovies()
-            .then((savedMoviesData) => {
-                if (savedMoviesData) {
-                    setSavedMovies(savedMoviesData);
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-
-        if (pathname === "/saved-movies") {
-            setVisibilityMoviesList("movies_visibility");
-        }
-    }, []);
-
-    function filterMovies(films) {
-        if (isShortFilms) {
-            return shortMoviesHandle(films);
-        }
-        return films.filter((movie) => movie.duration >= 40);
-    }
-
-    function definitionSizeScreen() {
-        return document.documentElement.clientWidth;
-    }
-
-    function coefficientScreen() {
-        const width = definitionSizeScreen();
-        if (width >= 1280) {
-            return 3;
-        }
-        return 2;
-    }
-
-    const filteredMovies = React.useMemo(() => filterMovies(movies), [
-        isShortFilms,
-        movies,
-    ]);
-    const filteredRenderedMovies = React.useMemo(
-        () => filterMovies(renderedFilms),
-        [isShortFilms, renderedFilms]
+  // Filter movies by keyword
+  function filterMoviesFromLS(moviesList) {
+    const films = moviesList.filter((movie) =>
+      movie.nameRU.includes(searchValue)
     );
-    const filteredSavedMovies = React.useMemo(() => filterMovies(savedMovies), [
-        isShortFilms,
-        savedMovies,
-    ]);
 
-    React.useEffect(() => {
-        if (filteredMovies.length >= filteredRenderedMovies.length) {
-            setVisibilityBtnYet("movies__button_hidden");
-        }
-    }, [filteredMovies, filteredRenderedMovies]);
+    setMoviesList(() => {
+      localStorage.setItem("foundFilms", JSON.stringify(films));
+      return films;
+    });
+  }
 
-
-    function countInitCards() {
-        const width = definitionSizeScreen();
-        if (width >= 1280) {
-            return 12;
-        }
-        if (width >= 757) {
-            return 8;
-        }
-        return 5;
+  //get films by keyword by clicking on Search
+  function handleSearch(evt) {
+    evt.preventDefault();
+    if (searchValue === "") {
+      setInputError("Нужно ввести ключевое слово");
+      return;
     }
-
-    function handleMoreRenderFilms() {
-        const cards = countInitCards();
-
-        setRenderedFilms(
-            filteredMovies.slice(0, cards + countClickMoreFilms * coefficientScreen())
-        );
-        setCountClickMoreFilms(countClickMoreFilms + 1);
-    }
-
-    // Filter movies by keyword
-    function filterMoviesFromLS(moviesList) {
-        const films = moviesList.filter((movie) =>
-            movie.nameRU.includes(searchValue)
-        );
-
-        setMoviesList(() => {
-            localStorage.setItem("foundFilms", JSON.stringify(films));
-            return films;
-        });
-    }
-
-    //get films by keyword by clicking on Search
-    function handleSearch(evt) {
-        evt.preventDefault();
-        if (searchValue === "") {
-            setInputError("Нужно ввести ключевое слово");
-            return;
-        }
-        // show preloader, hide movies
-        setIsPreloaderOpen("preloader_active");
-        setVisibilityMoviesList("");
-        if (pathname === "/movies") {
-            if (!localStorage.getItem("moviesList")) {
-                MoviesApi.getMovies()
-                    .then((moviesList) => {
-                        localStorage.setItem("moviesList", JSON.stringify(moviesList));
-                        filterMoviesFromLS(JSON.parse(localStorage.moviesList));
-                        setIsPreloaderOpen("");
-                        setVisibilityMoviesList("movies_visibility");
-                        setVisibilityBtnYet("");
-                    })
-                    .catch((err) => console.log(err));
-                return;
-            }
-
-            filterMoviesFromLS(
-                localStorage.getItem("moviesList")
-                    ? JSON.parse(localStorage.moviesList)
-                    : []
-            );
+    // show preloader, hide movies
+    setIsPreloaderOpen("preloader_active");
+    setVisibilityMoviesList("");
+    if (pathname === "/movies") {
+      if (!localStorage.getItem("moviesList")) {
+        MoviesApi.getMovies()
+          .then((moviesList) => {
+            localStorage.setItem("moviesList", JSON.stringify(moviesList));
+            filterMoviesFromLS(JSON.parse(localStorage.moviesList));
             setIsPreloaderOpen("");
             setVisibilityMoviesList("movies_visibility");
             setVisibilityBtnYet("");
-        } else {
-            setSavedMovies(
-                savedMovies.filter((movie) => movie.nameRU.includes(searchValue))
-            );
-            setVisibilityMoviesList("movies_visibility");
-            setIsPreloaderOpen("");
-        }
-    }
+          })
+          .catch((err) => console.log(err));
+        return;
+      }
 
-    function addMovie(movie) {
-        MainApi.addMovie(movie)
-            .then((dataMovie) => {
-                setSavedMovies([dataMovie.data, ...savedMovies]);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+      filterMoviesFromLS(
+        localStorage.getItem("moviesList")
+          ? JSON.parse(localStorage.moviesList)
+          : []
+      );
+      setIsPreloaderOpen("");
+      setVisibilityMoviesList("movies_visibility");
+      setVisibilityBtnYet("");
+    } else {
+      setSavedMovies(
+        savedMovies.filter((movie) => movie.nameRU.includes(searchValue))
+      );
+      setVisibilityMoviesList("movies_visibility");
+      setIsPreloaderOpen("");
     }
+  }
 
-    function removeMovie(movieId) {
-        MainApi.removeMovie(movieId)
-            .then(() => {
-                const newMovies = savedMovies.filter((movie) => movie._id !== movieId);
-                setSavedMovies(newMovies);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
+  function addMovie(movie) {
+    MainApi.addMovie(movie)
+      .then((dataMovie) => {
+        setSavedMovies([dataMovie.data, ...savedMovies]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    return (
-        <>
-            <Header bgColor="light" textColor="black" isLogin={isLogin}/>
-            <SearchForm
-                onSubmit={handleSearch}
-                searchValue={searchValue}
-                setSearchValue={setSearchValue}
-                inputError={inputError}
-                setInputError={setInputError}
-                isShortFilms={isShortFilms}
-                setIsShortFilms={setIsShortFilms}
-            />
-            <Preloader isPreloaderOpen={isPreloaderOpen}/>
-            <MoviesCardList
-                movies={filteredMovies}
-                visibilityMoviesList={visibilityMoviesList}
-                renderedFilms={filteredRenderedMovies}
-                setRenderedFilms={setRenderedFilms}
-                handleMoreRenderFilms={handleMoreRenderFilms}
-                countInitCards={countInitCards}
-                addMovie={addMovie}
-                removeMovie={removeMovie}
-                savedMovies={filteredSavedMovies}
-                setVisibilityMoviesList={setVisibilityMoviesList}
-                visibilityBtnYet={visibilityBtnYet}
-                setVisibilityBtnYet={setVisibilityBtnYet}
-                shortMoviesHandle={shortMoviesHandle}
-                isShortFilms={isShortFilms}
-            />
-            <Footer/>
-        </>
-    );
+  function removeMovie(movieId) {
+    MainApi.removeMovie(movieId)
+      .then(() => {
+        const newMovies = savedMovies.filter((movie) => movie._id !== movieId);
+        setSavedMovies(newMovies);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <>
+      <Header bgColor="light" textColor="black" isLogin={isLogin} />
+      <SearchForm
+        onSubmit={handleSearch}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        inputError={inputError}
+        setInputError={setInputError}
+        isShortFilms={isShortFilms}
+        setIsShortFilms={setIsShortFilms}
+      />
+      <Preloader isPreloaderOpen={isPreloaderOpen} />
+      <MoviesCardList
+        movies={filteredMovies}
+        visibilityMoviesList={visibilityMoviesList}
+        renderedFilms={filteredRenderedMovies}
+        setRenderedFilms={setRenderedFilms}
+        handleMoreRenderFilms={handleMoreRenderFilms}
+        countInitCards={countInitCards}
+        addMovie={addMovie}
+        removeMovie={removeMovie}
+        savedMovies={filteredSavedMovies}
+        setVisibilityMoviesList={setVisibilityMoviesList}
+        visibilityBtnYet={visibilityBtnYet}
+        setVisibilityBtnYet={setVisibilityBtnYet}
+        shortMoviesHandle={shortMoviesHandle}
+        isShortFilms={isShortFilms}
+      />
+      <Footer />
+    </>
+  );
 }
 
 export default Movies;
--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,80 +1,90 @@
 import React from "react";
 import "./MoviesCard.css";
-import {useLocation} from "react-router-dom";
-import moviesIconCard from "../../images/added-card-icon.svg";
-import moviesSavedCardIcon from "../../images/delete-card-icon.svg";
-import saveCardIcon from "../../images/save-card-icon.svg"
-import CurrentUserContext from '../../context/CurrentUserContext';
+import { useLocation } from "react-router-dom";
+import moviesIconCard from "../../images/saved_card.svg";
+import moviesSavedCardIcon from "../../images/delete_card.svg";
+import saveCardIcon from "../../images/save_card.svg";
+import CurrentUserContext from "../../context/CurrentUserContext";
 
 function MoviesCard({
-                        movie,
-                        cardName,
-                        cardDuration,
-                        imageLink,
-                        trailerLink,
-                        addMovie,
-                        removeMovie,
-                        savedMovies
-                    }) {
-    const currentUser = React.useContext(CurrentUserContext);
-    const [isAddedCard, setIsAddedCard] = React.useState(false);
+  movie,
+  cardName,
+  cardDuration,
+  imageLink,
+  trailerLink,
+  addMovie,
+  removeMovie,
+  savedMovies,
+}) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const [isAddedCard, setIsAddedCard] = React.useState(false);
 
-    const {pathname} = useLocation();
-    // If the movie is added to favorites
-    const moviesIcon = (isAddedCard ? moviesIconCard : saveCardIcon)
-    // If on the general search page, then show the icon defined on the line above, if not, then the delete icon
-    const cardIcon = (pathname === "/movies" ? moviesIcon : moviesSavedCardIcon)
+  const { pathname } = useLocation();
+  // If the movie is added to favorites
+  const moviesIcon = isAddedCard ? moviesIconCard : saveCardIcon;
+  // If on the general search page, then show the icon defined on the line above, if not, then the delete icon
+  const cardIcon = pathname === "/movies" ? moviesIcon : moviesSavedCardIcon;
 
-
-    function hadleLikeMovie() {
-        if (!isAddedCard) {
-            addMovie(movie)
-            setIsAddedCard(true)
-        } else {
-            const movieItem = savedMovies.filter((savedMovie) => savedMovie.movieId === movie._id);
-            removeMovie(movieItem[0].data._id)
-            console.log(movieItem[0].data)
-            setIsAddedCard(false)
-        }
+  function hadleLikeMovie() {
+    if (!isAddedCard) {
+      addMovie(movie);
+      setIsAddedCard(true);
+    } else {
+      const movieItem = savedMovies.filter(
+        (savedMovie) => savedMovie.movieId === movie._id
+      );
+      removeMovie(movieItem[0].data._id);
+      console.log(movieItem[0].data);
+      setIsAddedCard(false);
     }
+  }
 
-    function hadleDeleteButton() {
-        removeMovie(movie._id);
+  function hadleDeleteButton() {
+    removeMovie(movie._id);
+  }
+
+  React.useEffect(() => {
+    if (savedMovies.length > 0) {
+      if (!isAddedCard) {
+        setIsAddedCard(
+          savedMovies.some(
+            (savedMovie) =>
+              savedMovie.movieId === movie._id &&
+              savedMovie.owner === currentUser._id
+          )
+        );
+      } else {
+        setIsAddedCard(false);
+      }
     }
+  }, []);
+  const functionIcon =
+    pathname === "/movies" ? hadleLikeMovie : hadleDeleteButton;
 
-    React.useEffect(() => {
-        if (savedMovies.length > 0) {
-            if (!isAddedCard) {
-                setIsAddedCard(savedMovies.some(savedMovie =>
-                    savedMovie.movieId === movie._id && savedMovie.owner === currentUser._id));
-            } else {
-                setIsAddedCard(false)
-            }
-        }
-    }, []);
-    const functionIcon = (pathname === "/movies" ? hadleLikeMovie : hadleDeleteButton)
-
-    return (
-        <li className="card">
-
-            <div className="card__wrap">
-                <img className="card__icon" src={cardIcon} alt="icon" onClick={functionIcon}/>
-                <a
-                    className="card__trailer-link"
-                    href={trailerLink}
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <img className="card__image" src={imageLink} alt={cardName}/>
-                </a>
-            </div>
-            <div className="card__description">
-                <p className="card__name">{cardName}</p>
-                <p className="card__duration">{cardDuration}</p>
-            </div>
-
-        </li>
-    );
+  return (
+    <li className="card">
+      <div className="card__wrap">
+        <img
+          className="card__icon"
+          src={cardIcon}
+          alt="icon"
+          onClick={functionIcon}
+        />
+        <a
+          className="card__trailer-link"
+          href={trailerLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="card__image" src={imageLink} alt={cardName} />
+        </a>
+      </div>
+      <div className="card__description">
+        <p className="card__name">{cardName}</p>
+        <p className="card__duration">{cardDuration}</p>
+      </div>
+    </li>
+  );
 }
 
 export default MoviesCard;
