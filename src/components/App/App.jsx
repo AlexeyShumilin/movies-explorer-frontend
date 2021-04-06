7cceff4d409a6907a9445995555443797c13e925
Alexey Shumilin <shumil.alexey@gmail.com>
2021-04-06 19:24:50 +0300

    Registration and Authorisation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+
+import { Route, Switch, useHistory } from "react-router-dom";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import MainApi from "../../utils/MainApi";
 import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
 import "./App.css";
 import Movies from "../Movies/Movies";
 import Profile from "../Profile/Profile";
@@ -10,29 +13,68 @@ import Register from "../Register/Register";
 import NotFound from "../NotFound/NotFound";
 
 function App() {
+  const history = useHistory();
+  const [registrationError, setRegisteredError] = React.useState(false);
+  const [loginError, setLoginError] = React.useState(false);
+  const [isLogin, setIsLogin] = React.useState(false);
+
+  const handleLogin = (email, password) => {
+    MainApi.login(email, password)
+      .then((data) => {
+        if (data) {
+          setIsLogin(true);
+          history.push("/movies");
+        }
+      })
+      .catch((err) => {
+        setLoginError(true);
+        console.log(err);
+      });
+  };
+
+  function handleRegister(email, password, name) {
+    MainApi.register(email, password, name)
+      .then((data) => {
+        if (data) {
+          handleLogin(email, password);
+          history.push("/signin");
+        }
+      })
+      .catch(() => {
+        setRegisteredError(true);
+      });
+  }
+
   return (
     <div className="page">
       <Switch>
         <Route path="/" exact>
-          <Header bgColor="dark" textColor="white" />
+          <Header bgColor="dark" textColor="white" isLogin={isLogin} />
           <Main />
           <Footer />
         </Route>
         <Route path="/movies" exact>
-          <Header bgColor="light" textColor="black" />
+          <Header bgColor="light" textColor="black" isLogin={isLogin} />
           <Movies />
           <Footer />
         </Route>
         <Route path="/saved-movies" exact>
-          <Header bgColor="light" textColor="black" />
+          <Header bgColor="light" textColor="black" isLogin={isLogin} />
           <Movies />
           <Footer />
         </Route>
         <Route path="/profile" exact>
-          <Header bgColor="light" textColor="black" />
+          <Header bgColor="light" textColor="black" isLogin={isLogin} />
           <Profile />
         </Route>
+        <Route path="/signin" exact>
+          <Login handleLogin={handleLogin} loginError={loginError} />
+        </Route>
         <Route path="/signup" exact>
+          <Register
+            handleRegister={handleRegister}
+            registrationError={registrationError}
+          />
           <Register />
         </Route>
         <Route path="*">
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 import Form from "../Form/Form";
-import logo from "../../images/header-logo.svg";
+import logo from "../../images/logo_header.svg";
+import CallbackValidation from "../../helpers/CallbackValidation";
 
-function Login() {
+function Login({ handleLogin, loginError }) {
+  const formCallbackValidation = CallbackValidation();
+  const { email, password } = formCallbackValidation.values;
+
+  const submitHandle = (event) => {
+    event.preventDefault();
+    handleLogin(email, password);
+    formCallbackValidation.resetForm();
+  };
+
   return (
     <section className="login">
       <div className="login__container">
@@ -19,6 +29,10 @@ function Login() {
             route: "/signup",
             linkText: "Регистрация",
           }}
+          submitHandle={submitHandle}
+          validation={formCallbackValidation}
+          formName="login"
+          loginError={loginError}
         />
       </div>
     </section>
